8cf25049bd5d1a20a5977022e7ca21ee2af6cf16
Ishant Pal <mrishantsre1511ish@gmail.com>
2025-06-23 11:15:23 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,17 @@ import './App.css';
 import { useState } from 'react';
 import RegistrationPage from './components/auth/RegistrationPage/RegistrationPage';
 import Dashboard from './components/dashboard/Dashboard';
-import Logo from './components/ui/Logo/Logo.js';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="App">
-      {/* {!isLoggedIn ? (
+      {!isLoggedIn ? (
         <RegistrationPage onLoginSuccess={() => setIsLoggedIn(true)} />
       ) : (
         <Dashboard onLogout={() => setIsLoggedIn(false)} />
-      )} */}
-       <Dashboard/>
-       {/* <RegistrationPage /> */}
+      )}
     </div>
   );
 }
